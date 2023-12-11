b24f1777e6ab13626dbf1d61d75e5d5a44530cea
FXWmL <fanjingxian@tmu.edu.cn>
2023-12-11 10:42:20 +0800

    first commit

---
+++ R/add.R
@@ -0,0 +1,15 @@
+#' Add number
+#'
+#' Add together two number
+#'
+#' add @keywords
+#' @param x A number
+#' @param y A number
+#' @returns the sum of \code{x} and \code{y}
+#' @examples
+#' add(1,1)
+#' add(10,1)
+
+add <- function(x, y){
+  x + y
+}
---
+++ R/firstpk.R
@@ -0,0 +1,11 @@
+#' my first package
+#'
+#' The test packages provides some functions :
+#'
+#' @section  test Functions:
+#' The foo function ...
+#'
+#' @docType  package
+#' @name firstpk
+
+NULL
